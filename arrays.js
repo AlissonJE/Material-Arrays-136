68758e0fa16f82d15d25248a095fa1c406316e88
edwindev <erozog125@gmail.com>
2026-05-04 12:20:23 -0500

    Intro-arrays in js.ER

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// import { people } from './people.js'
+// console.log(people)
+
+const fruits = ['apple', 'banana', 'orange', 'grape']
+const names = ['John', 'Jane', 'Alice', 'Bob']
+const numbers = [1, 2, 3, 4, 5]
+const colors = ['red', 'green', 'blue', 'yellow']
+
+const animal = {
+    name: 'Dog',
+    species: 'Canine',
+    color: 'Brown',
+    skills: ['barking', 'fetching', 'guarding']
+}
+
+
+
---
+++ people.js
@@ -0,0 +1,18 @@
+export const people = [
+	{
+		name: 'John Doe',
+		age: 30
+	},
+	{
+		name: 'Jane Smith',
+		age: 25
+	},
+	{
+		name: 'Alice Johnson',
+		age: 28
+	},
+	{
+		name: 'Bob Brown',
+		age: 35
+	}
+]
